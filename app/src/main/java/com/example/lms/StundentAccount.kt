f2d459a2c66fead8dccb9d711fbe959971ae6e24
Eyntig <eugeneyntig54@gmail.com>
2024-02-15 02:33:37 +0800

    LMS project

---
+++ app/src/main/java/com/example/lms/StundentAccount.kt
@@ -0,0 +1,11 @@
+package com.example.lms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StundentAccount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stundent_account)
+    }
+}
